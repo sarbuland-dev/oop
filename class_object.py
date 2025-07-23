651da8fc4cef3c66c67fd0efc4e316f05c20c936
Sarbuland <meharsahab435@gmail.com>
2025-07-24 04:57:18 +0500

    oop
    oop

---
+++ class_object.py
@@ -0,0 +1,14 @@
+class car:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+        
+
+my_car=car("range rover","rx13")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car=car("tesla","nelon")
+print(my_new_car.brand)
+print(my_new_car.model)
+

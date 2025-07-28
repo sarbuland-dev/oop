a0f73baa17d5e7a71363bc56e73a34f169a42252
Sarbuland <meharsahab435@gmail.com>
2025-07-29 04:52:51 +0500

    oop
    oop

--- classes_self_object.py/practise.py
+++ classes_self_object.py/practise.py
@@ -14,7 +14,7 @@
 # (s1.details())
 # (s2.details())
 # (s3.details())
-#   2 question
+# #   2 question
 # class Car:
 #     def __init__(self,brand,model,car_number):
 #         self.brand=brand
@@ -33,23 +33,62 @@
 # print('____________________________')
 # car3=Car("lamo",2023,"Ajk 4540")
 # car3.details()
-# 3 question
+# # 3 question
 
-class Animal:
-    def __init__(self,name):
-        self.name=name
-    def details(self):
-        if self.name=="Dog":
-            return "bark"
-        elif self.name=="Cat":
-            return "mewo"
-        else:
-            return("not correct")
+# class Animal:
+#     def __init__(self,name):
+#         self.name=name
+#     def details(self):
+#         if self.name=="Dog":
+#             return "bark"
+#         elif self.name=="Cat":
+#             return "mewo"
+#         else:
+#             return("not correct")
         
-dog=Animal("Dog")
-print(dog.details())
-cat=Animal("er")
-print(cat.details())
+# dog=Animal("Dog")
+# print(dog.details())
+# cat=Animal("er")
+# print(cat.details())
+
+
+# class person:
+#     def __init__(self,name=None,age):
+#         self.name=name
+#         self.age=age
+#     def show(self):
+#         print("name:",self.name)
+#         print(" age",self.age)
+
+# s1=person("ali",21)
+# s1.show()
+
+# class book:
+#     def __init__(self,title,author="unkonwn"):
+#         self.title=title
+#         self.author=author
+#     def show(self):
+#         print(self.title)
+#         print(self.author)
+        
+# s1=book("love")
+# s1.show()
+
+
+class emploee:
+    company="goolge"
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def show(self):
+        print(self.name)
+        print(self.salary)
+        print(emploee.company)
+
+s1=emploee("ali",120000000)
+s1.show()
+
+
 
         
 
--- inheritance.py/practise_inheritance.py
+++ inheritance.py/practise_inheritance.py
@@ -34,17 +34,103 @@
 # s1.detail()
 # s1.show()
 
-class shape:
-    def area():
-        return 0
+# class shape:
+#     def area():
+#         return 0
     
-class Rectangle(shape):
-    def __init__(self,width,height):
-        self.width=width
-        self.height=height
-    def show(self):
-        print("area is ",self.width*self.height)
+# class Rectangle(shape):
+#     def __init__(self,width,height):
+#         self.width=width
+#         self.height=height
+#     def show(self):
+#         print("area is ",self.width*self.height)
 
-s=Rectangle(4,5)
-print(s.show())
+# s=Rectangle(4,5)
+# print(s.show())
         
+# class person:
+#     def __init__(self,name):
+#         self.name=name
+#     def show(self):
+#         print("name",self.name)
+
+# class teacher(person):
+#     def __init__(self,name,department):
+#         super().__init__(name)
+#         self.department=department
+#     def show(self):
+#       super().show()
+#       print("department",self.department)
+
+# class studentb(teacher):
+#     def __init__(self,name,department,id):
+#         super().__init__(name,department)
+#         self.id=id
+#     def show(self):
+#         super().show()
+#         print("id",self.id)
+
+
+# s1=studentb("ali","soft",123345)
+# s1.show()
+
+
+# class sportcar:
+#     def __init__(self,name,model):
+#         self.name=name
+#         self.model=model
+#     def details(self):
+#         print("name",self.name)
+#         print("model",self.model)
+
+# class SUV(sportcar):
+#     def __init__(self,name,model,speed):
+#         super().__init__(name,model)
+#         self.speed=speed
+#     def details(self):
+#         super().details()
+#         print("speed",self.speed)
+
+# class Electriccar(SUV):
+#     def __init__(self, name, model, speed,battery):
+#         super().__init__(name, model, speed)
+#         self.battery=battery
+#     def details(self):
+#          super().details()
+#          print("battery",self.battery)
+
+# s1=SUV("tesla","aun4567",230,)
+# s1.details()
+
+class shape:
+    def area(self):
+         return 0
+        
+
+class circle(shape):
+     def __init__(self,radius):
+          self.raduis=radius
+     def area(self):
+         return 3.14*self.raduis*self.raduis
+     
+class rectangle(circle):
+     def __init__(self,lenght,height):
+          self.lenght=lenght
+          self.height=height
+     def area(self):
+         return self.height*self.lenght
+     
+s1=rectangle(24,56)
+print(s1.area())
+s2=circle(34)
+print(s2.area())
+
+
+    
+
+
+        
+
+        
+
+    
